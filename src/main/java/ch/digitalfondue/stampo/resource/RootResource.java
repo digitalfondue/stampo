d6d53dd241bac2d5aa2d1c176eb7b36e47c881ee
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-03-27 19:05:18 +0100

    final cleanup

--- src/main/java/ch/digitalfondue/stampo/resource/RootResource.java
+++ src/main/java/ch/digitalfondue/stampo/resource/RootResource.java
@@ -17,15 +17,15 @@ package ch.digitalfondue.stampo.resource;
 
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
+import java.nio.file.DirectoryStream.Filter;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -33,7 +33,7 @@ public class RootResource implements Resource, Directory {
 
   private final Path path;
   private final ResourceFactory resourceFactory;
-
+  
 
   public RootResource(ResourceFactory resourceFactory, Path path) {
     this.path = path;
@@ -44,43 +44,32 @@ public class RootResource implements Resource, Directory {
     FileSystem fs = p.getFileSystem();
     return pathMatchers.stream().anyMatch(m -> fs.getPathMatcher(m).matches(p.getFileName()));
   }
-
-  private static <T extends Resource> Map<String, T> toMap(List<T> l) {
-    Map<String, T> m = new LinkedHashMap<>();//we care about insertion order
-    for (T t : l) {
-      m.put(t.getName(), t);
+      
+  private <T extends Resource> Map<String, T> fromDirectoryStream(Filter<Path> filter, Function<Path, T> mapper, Comparator<T> comparator) {
+    
+    //linkedhashmap as we want to preserve the insertion order
+    try (DirectoryStream<Path> dirs = Files.newDirectoryStream(path, filter)) {
+      return StreamSupport.stream(dirs.spliterator(), false)
+            .map(mapper)
+            .sorted(comparator)
+            .collect(Collectors.toMap(Resource::getName, Function.identity(), (k, v) -> {throw new IllegalStateException("duplicate key " + k);}, LinkedHashMap::new));
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
     }
-    return Collections.unmodifiableMap(m);
   }
 
   public Map<String, FileResource> getFiles() {
-    try (DirectoryStream<Path> dirs = Files.newDirectoryStream(path, (p) -> Files.isRegularFile(p) && !mustBeIgnored(p, resourceFactory.getConfiguration().getIgnorePatterns()))) {
-      
-      List<FileResource> fs = StreamSupport.stream(dirs.spliterator(), false)
-           .map(p -> resourceFactory.fileResource(p, this))
-           .sorted(resourceFactory.getFileResourceComparator())
-           .collect(Collectors.toList());
-      
-      return toMap(fs);
-      
-    } catch (IOException e) {
-      throw new IllegalStateException(e);
-    }
+    
+    return fromDirectoryStream(
+        (p) -> Files.isRegularFile(p) && !mustBeIgnored(p, resourceFactory.getConfiguration().getIgnorePatterns()), 
+        p -> resourceFactory.fileResource(p, this), 
+        resourceFactory.getFileResourceComparator());
   }
 
   public Map<String, Directory> getDirectories() {
-    try (DirectoryStream<Path> dirs = Files.newDirectoryStream(path, Files::isDirectory)) {
-      
-      List<Directory> ds = StreamSupport.stream(dirs.spliterator(), false)
-          .map(p -> resourceFactory.directory(p, this))
-          .sorted(Comparator.comparing(Directory::getName))
-          .collect(Collectors.toList());
-      
-      return toMap(ds);
-      
-    } catch (IOException e) {
-      throw new IllegalStateException(e);
-    }
+    
+    return fromDirectoryStream(Files::isDirectory, p -> resourceFactory.directory(p, this), Comparator.comparing(Directory::getName));
+    
   }
 
   @Override
