5a83170bbb42d9d4895a0e6f657b11afadd9b70e
Eros Taborelli <erost@digitalfondue.ch>
2015-06-13 01:24:31 +0200

    cleanup

--- src/test/java/ch/digitalfondue/stampo/NewCommandTest.java
+++ src/test/java/ch/digitalfondue/stampo/NewCommandTest.java
@@ -15,10 +15,20 @@
  */
 package ch.digitalfondue.stampo;
 
+import static java.nio.file.Files.delete;
+import static java.nio.file.Files.exists;
+import static java.nio.file.Files.isDirectory;
+import static java.nio.file.Files.walkFileTree;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -31,14 +41,26 @@ public class NewCommandTest {
 		return strings;
 	}
 	
-	private void deleteProjectDirectory(File path) {
-		if(!path.exists()) return;
-		if(path.isDirectory()) {
-			for (File f : path.listFiles()){
-				deleteProjectDirectory(f);
-            }
+	private void cleanUp(Path path) {
+		if (exists(path) && isDirectory(path)) {
+			try {
+				walkFileTree(path, new SimpleFileVisitor<Path>() {
+					@Override
+					public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+						delete(file);
+						return FileVisitResult.CONTINUE;
+					}
+
+					@Override
+					public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+						delete(dir);
+						return FileVisitResult.CONTINUE;
+					}
+				});
+			} catch (IOException e) {
+				throw new IllegalStateException(e);
+			}
 		}
-		path.delete();
 	}
 	
 	@Test
@@ -58,16 +80,16 @@ public class NewCommandTest {
 	    Assert.assertFalse(n.getPath().isPresent());
 	    Assert.assertTrue(n.getName().isPresent());
 	    
-	    n.run();
-	    
-	    //assert
-	    Assert.assertTrue(Paths.get("./test/test.txt").toFile().exists());
-	    Assert.assertTrue(Paths.get("./test/child").toFile().exists());
-	    Assert.assertTrue(Paths.get("./test/child").toFile().isDirectory());
-	    Assert.assertTrue(Paths.get("./test/child/test-child.txt").toFile().exists());
-	    
-	    //cleanup
-	    deleteProjectDirectory(Paths.get("./test").toFile());
+	    try {
+	    	n.run();
+
+	    	Assert.assertTrue(Paths.get("./test/test.txt").toFile().exists());
+	    	Assert.assertTrue(Paths.get("./test/child").toFile().exists());
+	    	Assert.assertTrue(Paths.get("./test/child").toFile().isDirectory());
+	    	Assert.assertTrue(Paths.get("./test/child/test-child.txt").toFile().exists());
+	    } finally {
+	    	cleanUp(Paths.get("./test"));
+	    }
 	}
 	
 	@Test
@@ -78,16 +100,16 @@ public class NewCommandTest {
 	    Assert.assertTrue(n.getPath().isPresent());
 	    Assert.assertFalse(n.getName().isPresent());
 	    
-	    n.run();
+	    try {
+	    	n.run();
 	    
-	    //assert
-	    Assert.assertTrue(Paths.get("./stampo-path/test.txt").toFile().exists());
-	    Assert.assertTrue(Paths.get("./stampo-path/child").toFile().exists());
-	    Assert.assertTrue(Paths.get("./stampo-path/child").toFile().isDirectory());
-	    Assert.assertTrue(Paths.get("./stampo-path/child/test-child.txt").toFile().exists());
-	    
-	    //cleanup
-	    deleteProjectDirectory(Paths.get("./stampo-path").toFile());
+	    	Assert.assertTrue(Paths.get("./stampo-path/test.txt").toFile().exists());
+	    	Assert.assertTrue(Paths.get("./stampo-path/child").toFile().exists());
+	    	Assert.assertTrue(Paths.get("./stampo-path/child").toFile().isDirectory());
+	    	Assert.assertTrue(Paths.get("./stampo-path/child/test-child.txt").toFile().exists());
+	    } finally {
+	    	cleanUp(Paths.get("./stampo-path"));
+	    }
 	}
 	
 	@Test
@@ -98,19 +120,19 @@ public class NewCommandTest {
 	    Assert.assertTrue(n.getPath().isPresent());
 	    Assert.assertFalse(n.getName().isPresent());
 	    
-	    n.run();
-	    
-	    //assert
-	    Assert.assertTrue(Paths.get("./stampo-path/test.txt").toFile().exists());
-	    Assert.assertTrue(Paths.get("./stampo-path/child").toFile().exists());
-	    Assert.assertTrue(Paths.get("./stampo-path/child").toFile().isDirectory());
-	    Assert.assertTrue(Paths.get("./stampo-path/child/test-child.txt").toFile().exists());
-	    Assert.assertTrue(Paths.get("./stampo-path/site").toFile().exists());
-	    Assert.assertTrue(Paths.get("./stampo-path/site").toFile().isDirectory());
-	    Assert.assertTrue(Paths.get("./stampo-path/site/site-test.txt").toFile().exists());
-	    
-	    //cleanup
-	    deleteProjectDirectory(Paths.get("./stampo-path").toFile());
+	    try {
+		    n.run();
+		    
+		    Assert.assertTrue(Paths.get("./stampo-path/test.txt").toFile().exists());
+		    Assert.assertTrue(Paths.get("./stampo-path/child").toFile().exists());
+		    Assert.assertTrue(Paths.get("./stampo-path/child").toFile().isDirectory());
+		    Assert.assertTrue(Paths.get("./stampo-path/child/test-child.txt").toFile().exists());
+		    Assert.assertTrue(Paths.get("./stampo-path/site").toFile().exists());
+		    Assert.assertTrue(Paths.get("./stampo-path/site").toFile().isDirectory());
+		    Assert.assertTrue(Paths.get("./stampo-path/site/site-test.txt").toFile().exists());
+	    } finally {   
+	    	cleanUp(Paths.get("./stampo-path"));
+	    }
 	}
 	
 	@Test
@@ -123,7 +145,6 @@ public class NewCommandTest {
 	    
 	    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	    System.setErr(new PrintStream(outContent));
-	    
 	    
 	    n.run();
 	    Assert.assertEquals("invalid archetype, specify one of: [basic, site, blog, doc]", outContent.toString().replaceAll("[\r\n]", ""));
